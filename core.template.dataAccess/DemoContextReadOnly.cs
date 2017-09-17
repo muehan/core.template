be120f037cf892d7356d3fc9eb5b248daa0cd3eb
André <tdc@gmx.ch>
2017-09-17 15:35:09 +0200

    context changed

--- core.template.dataAccess/DemoContextReadOnly.cs
+++ core.template.dataAccess/DemoContextReadOnly.cs
@@ -1,20 +1,9 @@
 ﻿namespace core.template.dataAccess
 {
-    using core.template.domain;
-    using Microsoft.EntityFrameworkCore;
     using System;
 
-    public class DemoContextReadOnly : DbContext
+    public class DemoContextReadOnly : DemoContext
     {
-        public DbSet<Customer> Customers { get; }
-
-        public DbSet<Order> Orders { get; }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer(@"Server=65efd7f8-e33c-4e12-9b37-a7f000ec3b02.sqlserver.sequelizer.com;Database=db65efd7f8e33c4e129b37a7f000ec3b02;User ID=iookhqsevwmafwyf;Password=AyU2CeaCuLkFdXZWkd4aeiXCsWkAdoYoKfqkjUcoGdeNBmgbbNYi77hrHqHP2Gyg;MultipleActiveResultSets=True;");
-        }
-
         public override int SaveChanges()
         {
             throw new InvalidOperationException("This is a Read-Only Context");
